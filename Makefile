c388f30ddd842a826bb2dd07fa736dd7cc0c7286
Stefan Jumarea <stefanjumarea02@gmail.com>
2023-07-02 17:57:04 +0300

    Add Makefile, configuration files and scripts
    Update the README with instructions on setting up, configuring, building
    and running the httpreply app with Unikraft. As part of it, add Makefile
    files, configuration files and scripts; they are referenced in the
    `README.md` file.
    
    Current instructions cover the use of QEMU/KVM platform (both on x86_64 and
    on AArch64) and GCC. Firecraker/KVM, Xen, Linux platforms and Clang are
    not yet documented.
    
    Signed-off-by: Stefan Jumarea <stefanjumarea02@gmail.com>

---
+++ Makefile
@@ -0,0 +1,9 @@
+UK_ROOT ?= $(PWD)/.unikraft/unikraft
+UK_LIBS ?= $(PWD)/.unikraft/libs
+LIBS := $(UK_LIBS)/lwip
+
+all:
+	@$(MAKE) -C $(UK_ROOT) A=$(PWD) L=$(LIBS)
+
+$(MAKECMDGOALS):
+	@$(MAKE) -C $(UK_ROOT) A=$(PWD) L=$(LIBS) $(MAKECMDGOALS)
---
+++ run-qemu-x86_64.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sudo ip link set dev virbr0 down 2> /dev/null
+sudo ip link del dev virbr0 2> /dev/null
+sudo ip link add dev virbr0 type bridge
+sudo ip address add 172.44.0.1/24 dev virbr0
+sudo ip link set dev virbr0 up
+
+sudo /usr/bin/qemu-system-x86_64 \
+    -netdev bridge,id=en0,br=virbr0 -device virtio-net-pci,netdev=en0 \
+    -append "netdev.ipv4_addr=172.44.0.2 netdev.ipv4_gw_addr=172.44.0.1 netdev.ipv4_subnet_mask=255.255.255.0 --" \
+    -kernel build/httpreply_qemu-x86_64 \
+    -nographic
